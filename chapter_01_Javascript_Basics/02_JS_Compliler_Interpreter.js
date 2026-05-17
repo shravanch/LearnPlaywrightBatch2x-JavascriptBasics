82df4931071713ef01d25129f51670e4fb39eeb9
shravanch <34641848+shravanch@users.noreply.github.com>
2026-05-17 21:12:23 +0530

    Fix comments for compiler and interpreter explanation
    Corrected comments to clarify compiler behavior.

--- chapter_01_Javascript_Basics/02_JS_Compliler_Interpreter.js
+++ chapter_01_Javascript_Basics/02_JS_Compliler_Interpreter.js
@@ -1,8 +1,8 @@
 let a = 10;
 console.log(a); // interpreter will execute this line and print 10
 
-for (let i = 0; i < 1000000; i++) {
-  // compiler will compile this loop and print 999999
+// ↓ compiler will compile this below loop and print 999999 ↓
+for (let i = 0; i < 1000000; i++) { 
   console.log(i);
   print();
 }
