1a6367c174b977419047a935059ba071db4849e0
Shravan Chilumula <shravan.chilumula@qvantel.com>
2026-05-17 20:41:27 +0530

    chapter_01_Javascript_Basics

---
+++ chapter_01_Javascript_Basics/01_Basics.js
@@ -0,0 +1,7 @@
+console.log("Hello The Testing Academy");
+let a = 10;
+console.log(a);
+let b = 20;
+console.log(b);
+let c = a + b;
+console.log(c);
---
+++ chapter_01_Javascript_Basics/02_JS_Compliler_Interpreter.js
@@ -0,0 +1,6 @@
+let a = 10;
+console.log(a);
+
+for (let i = 0; i < 1000000; i++) {
+    console.log(i);
+}
