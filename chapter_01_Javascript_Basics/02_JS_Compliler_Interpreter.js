3ae177d04e7a33c1a2d66b2588aeb5cc0199dc65
Shravan Chilumula <shravan.chilumula@qvantel.com>
2026-05-17 21:09:42 +0530

    chapter_01_Javascript_Basics

--- chapter_01_Javascript_Basics/02_JS_Compliler_Interpreter.js
+++ chapter_01_Javascript_Basics/02_JS_Compliler_Interpreter.js
@@ -1,6 +1,11 @@
 let a = 10;
-console.log(a);
+console.log(a); // interpreter will execute this line and print 10
 
 for (let i = 0; i < 1000000; i++) {
-    console.log(i);
+  // compiler will compile this loop and print 999999
+  console.log(i);
+  print();
+}
+function print() {
+  console.log("Hello");
 }
